862c2836c0edf9f2b6635ecd4b5f8d7ef10e0ea4
Mahesh More <maheshmore667@gmail.com>
2023-10-14 21:21:36 +0530

    get one course functionality implemented

--- 21buildApi/main.go
+++ 21buildApi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Courses struct {
@@ -39,4 +41,20 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	//grab id from request
+	params := mux.Vars(r)
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+
+	}
+	json.NewEncoder(w).Encode("No course found with the given Id")
+	return
 }
