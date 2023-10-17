710781d56660c641178ff7ff323d90afe0952190
Mahesh More <maheshmore667@gmail.com>
2023-10-17 18:47:41 +0530

    update course method is developed

--- 21buildApi/main.go
+++ 21buildApi/main.go
@@ -79,3 +79,29 @@ func cerateOneCourse(w http.ResponseWriter, r *http.Request) {
 	return
 
 }
+
+func updateCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	var course Courses
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Empty Payload")
+	}
+	json.NewDecoder(r.Body).Decode(&course)
+	isCoursePresent := false
+	for index, courseInfo := range courses {
+		if courseInfo.CourseId == course.CourseId {
+			isCoursePresent = true
+			courses = append(courses[:index], courses[index+1:]...)
+			var newCourse Courses
+			newCourse.CourseId = course.CourseId
+			courses = append(courses, newCourse)
+			json.NewEncoder(w).Encode(newCourse)
+			return
+		}
+	}
+	if !isCoursePresent {
+		json.NewEncoder(w).Encode("No Course is present with give courseId")
+		return
+	}
+
+}
